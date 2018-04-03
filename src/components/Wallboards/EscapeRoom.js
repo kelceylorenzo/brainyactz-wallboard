86add87c6d35f14709e7edc2c685f3733e410b9e
m13kelore <kelceylorenzo@gmail.com>
2018-04-03 16:54:51 -0700

    completed adding background images to escape room boards dynalically

--- src/components/Wallboards/EscapeRoom.js
+++ src/components/Wallboards/EscapeRoom.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 export default (props) => {
+	console.log(props);
+	let style = {
+		backgroundImage: `url(${props.backgroundImage})`,
+		backgroundRepeat: 'no-repeat',
+		backgroundSize: 'cover'
+	};
+
 	return (
-		<div className="page-container escape-room">
-			<div className="page-title escape-room">{props.title}</div>
-			<div className="page-subtitle escape-room">{props.subtitle}</div>
-			<div className="page-content escape-room">Optional Video</div>
-			<div className="page-content escape-room">Leaderboard</div>
+		<div className="page-container" style={style}>
+			<div className="page-container escape-room">
+				<div className="page-title escape-room">{props.title}</div>
+				<div className="page-subtitle escape-room">{props.subtitle}</div>
+				<div className="page-content escape-room">Optional Video</div>
+				<div className="page-content escape-room">Leaderboard</div>
+			</div>
 		</div>
 	);
 };
